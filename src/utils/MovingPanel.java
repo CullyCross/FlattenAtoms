10f59baf53de48190a8594a7b2603f03d8e0646f
CullyCross <elaugfein@gmail.com>
2015-03-05 00:07:36 +0200

    -New version

--- src/utils/MovingPanel.java
+++ src/utils/MovingPanel.java
@@ -10,12 +10,13 @@ public class MovingPanel extends JPanel {
 
     private final int D = 200; //diameter
     private final int X = 40; //x coord
-    private final float COEFFICIENT = (float)((4f/3f) * 0.07 * Math.sqrt(D/2));
+    //private final float COEFFICIENT = (float)((4f/3f) * 0.07 * Math.sqrt(D/2));
+    private final float COEFFICIENT = (float)Math.sqrt(D/2);
 
-    private int yFirst = 40;
-    private int ySecond = yFirst + D + 10;
+    private float yFirst = 40;
+    private float ySecond = yFirst + D + 10;
 
-    private final int yCenter = ySecond - yFirst + (int)(0.175 * D); //MAGIC CONSTANTS
+    private final float yCenter = ySecond - yFirst + (int)(0.175 * D); //MAGIC CONSTANTS
 
     public static volatile double F;
     public static volatile double d;
@@ -34,12 +35,12 @@ public class MovingPanel extends JPanel {
         update();
 
         g.setColor(Color.BLUE);
-        g.fillOval(X, yFirst,
+        g.fillOval(X, (int)yFirst,
                 D, D);
-        g.fillOval(X, ySecond,
+        g.fillOval(X, (int)ySecond,
                 D, D);
         g.setColor(Color.BLACK);
-        g.drawLine(X/2, yCenter, (int)(6.5*X), yCenter);
+        g.drawLine(X/2, (int)yCenter, (int)(6.5*X), (int)yCenter);
     }
 
     private void update() {
@@ -48,8 +49,8 @@ public class MovingPanel extends JPanel {
         F = COEFFICIENT * d * Math.sqrt(d);
 
         if(d < 0.2 * (D/2)) {
-            yFirst++;
-            ySecond--;
+            yFirst += 0.5f;
+            ySecond -= 0.5f;
         } else {
             sRun = false;
         }
